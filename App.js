a8f1aa05632044f8ed9886aa007e4e97bb5aaf89
Binit <dspmurranchi@gmail.com>
2024-05-14 09:24:08 +0530

    episode01

---
+++ App.js
@@ -0,0 +1,7 @@
+
+
+    const heading = React.createElement("h1" ,{}, "Hello World");
+    const nextheading =React.createElement("h2" ,{}, "Bro");
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
+   
